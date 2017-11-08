199c0c38c88d263dd11c47441d5503850ae313d7
psarma89 <priyam.sarma@gmail.com>
2017-11-08 10:46:22 -0500

    Adds solution for student scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,48 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
+    students = html.css(".student-card")
+    students.collect do |student|
+      {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      }
+    end
+    # binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = Nokogiri::HTML(open(profile_url))
+    # html.css(".social-icon-container a").attribute("href").value
+    profile_hash = {
+      # :twitter=> "",
+      # :linkedin=> "",
+      # :github=> "",
+      # :blog=> "",
+      :profile_quote=> html.css(".profile-quote").text,
+      :bio=> html.css(".description-holder p").text
+    }
+
+    html.css(".social-icon-container a").each do |social|
+      social_url = social.attribute("href").value
+      if social_url.include?("twitter")
+        profile_hash[:twitter] = social_url
+      elsif social_url.include?("linkedin")
+        profile_hash[:linkedin] = social_url
+      elsif social_url.include?("github")
+        profile_hash[:github] = social_url
+      else
+        profile_hash[:blog] = social_url
+      end
+    end
+
+    profile_hash
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,34 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(student_hash={})
+    student_hash.each{|k,v| self.send("#{k}=", v)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.collect do |student_array|
+      student = Student.new(student_array)
+      # student.name = students_array[:name]
+      # student.location = students_array[:location]
+      student.twitter = []
+      student.linkedin = []
+      student.github = []
+      student.blog = []
+      student.profile_quote = []
+      student.bio = []
+      student.profile_url = []
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each{|k,v| self.send("#{k}=", v)}
   end
 
   def self.all
-    
+    @@all
   end
 end
-
